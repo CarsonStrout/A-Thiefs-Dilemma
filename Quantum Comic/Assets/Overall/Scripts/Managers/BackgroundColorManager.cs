fa74e22c0815a6175e26610b3ccf6fd093095d36
Carson Strout <carson.strout42@gmail.com>
2023-04-16 15:18:00 -0600

    More panels, color transitions for section 3

---
+++ Quantum Comic/Assets/Overall/Scripts/Managers/BackgroundColorManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColorManager : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Color[] colors;
+    private int colorPos;
+    private int nextColorPos;
+    [SerializeField] private float duration = 1.5F;
+
+    private void Start()
+    {
+        colorPos = PlayerPrefs.GetInt("PageNumber", 0);
+        cam.clearFlags = CameraClearFlags.SolidColor;
+    }
+
+    private void Update()
+    {
+        cam.backgroundColor = Color.Lerp(cam.backgroundColor, colors[nextColorPos], duration * Time.deltaTime);
+    }
+
+    public void NextColor()
+    {
+        nextColorPos = colorPos + 1;
+        colorPos++;
+    }
+
+    public void PreviousColor()
+    {
+        nextColorPos = colorPos - 1;
+        colorPos--;
+    }
+}
