748af2fb802bb128ef82532cd3fbea981153cf83
Carson Strout <carson.strout42@gmail.com>
2023-04-23 14:07:32 -0600

    Section 3 panels, game 2 sounds and obstacles

--- Quantum Comic/Assets/Game 2/Scripts/Players/GameOver.cs
+++ Quantum Comic/Assets/Game 2/Scripts/Players/GameOver.cs
@@ -6,17 +6,44 @@ public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameManager2 gameManager2;
     [SerializeField] private GameObject otherPlayer;
-    [SerializeField] private ParticleSystem deathParticle;
+
+    [Header("Particle Effects")]
+    [SerializeField] private ParticleSystem boxParticle;
+    [SerializeField] private ParticleSystem mineParticle;
+    [SerializeField] private ParticleSystem implosionParticle;
+
+    [Space(5)]
+    [Header("Death Sounds")]
     [SerializeField] private AudioSource playerExplosion;
+    [SerializeField] private AudioSource mineExplosion;
+    [SerializeField] private AudioSource playerImplosion;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Obstacle")
+        if (other.gameObject.tag == "Box")
         {
             playerExplosion.Play();
-            Instantiate(deathParticle, otherPlayer.transform.position, deathParticle.transform.rotation);
+            Instantiate(boxParticle, otherPlayer.transform.position, boxParticle.transform.rotation);
             otherPlayer.SetActive(false);
-            Instantiate(deathParticle, gameObject.transform.position, deathParticle.transform.rotation);
+            Instantiate(boxParticle, gameObject.transform.position, boxParticle.transform.rotation);
+            gameObject.SetActive(false);
+            gameManager2.loseGame = true;
+        }
+        else if (other.gameObject.tag == "Mine")
+        {
+            mineExplosion.Play();
+            Instantiate(mineParticle, otherPlayer.transform.position, mineParticle.transform.rotation);
+            otherPlayer.SetActive(false);
+            Instantiate(mineParticle, gameObject.transform.position, mineParticle.transform.rotation);
+            gameObject.SetActive(false);
+            gameManager2.loseGame = true;
+        }
+        else if (other.gameObject.tag == "Obstacle")
+        {
+            playerImplosion.Play();
+            Instantiate(implosionParticle, otherPlayer.transform.position, implosionParticle.transform.rotation);
+            otherPlayer.SetActive(false);
+            Instantiate(implosionParticle, gameObject.transform.position, implosionParticle.transform.rotation);
             gameObject.SetActive(false);
             gameManager2.loseGame = true;
         }
