b806115ac5b6e6dea75560602d52a1c222470ad5
Carson Strout <carson.strout42@gmail.com>
2023-04-25 01:44:55 -0600

    Added 'final' animations

--- Quantum Comic/Assets/Comic 3/Scripts/ComicReverse.cs
+++ Quantum Comic/Assets/Comic 3/Scripts/ComicReverse.cs
@@ -5,17 +5,20 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using Cinemachine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class ComicReverse : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera cm;
+    [SerializeField] private VideoPlayer panel;
     [SerializeField] private Animator transition;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioSource rewindSound;
     [SerializeField] private Volume pp; // urp post processing
     [SerializeField] private GameObject button;
     [SerializeField] private SoundFadeManager soundFadeManager;
+    [SerializeField] private GameObject instructionText;
 
     private ChromaticAberration chromaticAberration;
     private Bloom bloom;
@@ -36,6 +39,9 @@ public class ComicReverse : MonoBehaviour
     private void Start()
     {
         timer = 0;
+        panel.playbackSpeed = 0;
+
+        instructionText.SetActive(true);
 
         if (rewindSound.isPlaying)
             rewindSound.Stop();
@@ -51,6 +57,8 @@ public class ComicReverse : MonoBehaviour
     {
         if (isRewinding)
         {
+            panel.playbackSpeed = Mathf.Lerp(panel.playbackSpeed, 1, (transitionTime * 3) * Time.deltaTime);
+
             chromaticAberration.intensity.value = Mathf.Lerp(chromaticAberration.intensity.value, chromMax, transitionTime * Time.deltaTime);
             bloom.intensity.value = Mathf.Lerp(bloom.intensity.value, bloomMax, transitionTime * Time.deltaTime);
             colorAdjustments.hueShift.value = Mathf.Lerp(colorAdjustments.hueShift.value, colorMax, transitionTime * Time.deltaTime);
@@ -61,15 +69,16 @@ public class ComicReverse : MonoBehaviour
             if (timer > 1.5)
                 soundFadeManager.FadeAudio();
 
-            if (timer > 2)
+            if (timer > 3)
                 transition.SetTrigger("Start");
 
-            if (timer > 3)
+            if (timer > 4)
                 SceneManager.LoadScene(5);
         }
 
         if (cm.isActiveAndEnabled && Input.GetKeyDown(KeyCode.LeftShift) && !isRewinding)
         {
+            instructionText.SetActive(false);
             button.SetActive(false);
             if (!rewindSound.isPlaying)
                 rewindSound.Play();
